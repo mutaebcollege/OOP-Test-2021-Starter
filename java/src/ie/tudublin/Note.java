1134df3af9cf6b6224c0c5ec300134ebd4d7af80
MQ <mq@MQs-MacBook-Pro.local>
2021-04-21 18:20:43 +0300

    setting the note class constructor and toString with getters and setters

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -4,4 +4,41 @@ public class Note {
 
   private char note;
   private int duration;
+
+  public Note() {}
+
+  public Note(char note, int duration) {
+    this.note = note;
+    this.duration = duration;
+  }
+
+  public char getNote() {
+    return this.note;
+  }
+
+  public void setNote(char note) {
+    this.note = note;
+  }
+
+  public int getDuration() {
+    return this.duration;
+  }
+
+  public void setDuration(int duration) {
+    this.duration = duration;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "{" +
+      " note='" +
+      getNote() +
+      "'" +
+      ", duration='" +
+      getDuration() +
+      "'" +
+      "}"
+    );
+  }
 }
